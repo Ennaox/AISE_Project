714e3836d4cee6634181b13aed92a62b5cc81d43
Ennaox <alexis.laplanche@hotmail.fr>
2022-02-21 15:41:40 +0100

    rm -rf + ajout fonction attach detach a un processus

--- main.c
+++ main.c
@@ -13,86 +13,94 @@
 
 unw_addr_space_t as;
 struct UPT_info *ui;
-int cdt = 1;
+unw_cursor_t cursor;
 
-void on_segfault(int sig)
-{
-	printf("Erreur de segmation c'est relou\n");
-	exit(1);
-}
-
-void child_ready(int sig)
-{
-	cdt = 0;
-}
-
-void backtrace(pid_t child)
+int attach(pid_t child)
 {
 	ui = _UPT_create(child);
 	if (!ui) 
 	{
        	printf("_UPT_create failed\n");
+       	return 5;
    	}
-	ptrace(PTRACE_ATTACH, child, 0, 0);
-	unw_cursor_t cursor;
+	int init_state = unw_init_remote(&cursor,as,&child);
 
-	int remote_cursor = unw_init_remote(&cursor, as, ui);
-	 if (remote_cursor != 0) {
-        if (remote_cursor == UNW_EINVAL) {
-            printf("unw_init_remote: UNW_EINVAL\n");
-        } else if (remote_cursor == UNW_EUNSPEC) {
-            printf("unw_init_remote: UNW_EUNSPEC\n");
-        } else if (remote_cursor == UNW_EBADREG) {
-            printf("unw_init_remote: UNW_EBADREG\n");
-        } else {
-            printf("unw_init_remote: UNKNOWN\n");
-        }
-    }
-    wait(NULL);
-
-	while (unw_step(&cursor) > 0)
+	if (init_state != 0) 
 	{
-		unw_word_t offset, reg;
-       	char func_name[256];
-       	memset(func_name,'\0',256*sizeof(char));
-       	
-       	unw_get_reg(&cursor, UNW_REG_IP, &reg);
-       	unw_get_proc_name(&cursor, func_name, 256*sizeof(char), &offset);
-		printf("%p : (%s+0x%x) [%p]\n", (void *)reg,
-                                          func_name,
-                                          (int) offset,
-                                          (void *) reg);
-	}
+        _UPT_destroy(ui);
+        if (-init_state == UNW_EINVAL) 
+        {
+            printf("unw_init_remote: UNW_EINVAL\n");
+            return 1;
+        } 
+        else if (-init_state == UNW_EUNSPEC) 
+        {
+            printf("unw_init_remote: UNW_EUNSPEC\n");
+            return 2;
+        } 
+        else if (-init_state == UNW_EBADREG) 
+        {
+            printf("unw_init_remote: UNW_EBADREG\n");
+            return 3;
+        } 
+        else 
+        {
+	        printf("unw_init_remote: UNKNOWN\n");
+	        return 4;
+	    }
+    }
 
-	ptrace(PTRACE_DETACH, child, 0, 0);
+    return 0;
+}
+
+int detach(pid_t child)
+{
+	long status = ptrace(PTRACE_DETACH, child, 0, 0);
+	if(status==-1)
+	{
+		printf("Error on PATRACE_DETACH\n");
+		return 6;
+	}
 	_UPT_destroy(ui);
 }
 
-int main(int argc,char **argv)
+int main(int argc, char *argv[])
 {
-	signal(SIGSEGV,on_segfault);
-	if(argc<2)
+	if(argc < 2)
 	{
-		printf("Need argument\n");
-		exit(2);
+		perror("USAGE: Need argument\n");
+		exit(120);
 	}
-	as = unw_create_addr_space(&_UPT_accessors, 0);
-	pid_t child = 0;
-	child = fork();
 	
+	pid_t child = 0;
+
+	as = unw_create_addr_space(&_UPT_accessors,0);
+
+	child = fork();
 	if(child)
 	{
-		printf("j'attend %u\n",getpid());
-		raise(SIGSTOP);
-		printf("j'attend plus\n");
-		backtrace(child);
-		wait(NULL);
+		sleep(1);
+		int status = 0;
+		if(status = attach(child))
+		{
+			kill(child,SIGINT);
+			return status;
+		}
+
+    	status = detach(child);
+    	if(status)
+    	{
+    		kill(child,SIGINT);
+    		return status;
+    	}
+		
+		int err = waitpid(child,NULL,0);
 	}
 	else
-	{	
-		kill(SIGCONT,getppid());
-		printf("Papa attend plus %u\n",getppid());
-		execvp(argv[1],argv+sizeof(char*));
+	{
+		ptrace(PTRACE_TRACEME,0,NULL,NULL);
+		execvp(argv[1],argv+sizeof(char *));
+		return 0;
 	}
 	return 0;
 }
