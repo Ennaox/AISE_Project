5b14438d71a05964e258ceea9cae0562bea9d3a6
Ennaox <alexis.laplanche@hotmail.fr>
2022-03-17 16:55:05 +0100

    Ajout commande info

--- crash.c
+++ crash.c
@@ -6,6 +6,14 @@
 
 void test()
 {
+
+	double a = 1.1;
+	double b = 1.2;
+	double c = 1.3;
+	double d = 1.4;
+	double e = 1.5;
+	double f = 1.6;
+	
 	__asm__ volatile
 	(
 		"mov $1, %%rax;\n"
@@ -22,14 +30,25 @@ void test()
 		"mov $14, %%r13;\n"
 		"mov $15, %%r14;\n"
 		"mov $16, %%r15;\n"
+		// "movss (%[_a]), %%xmm0;\n"
+		// "movss (%[_b]), %%xmm1;\n"
+		// "movss (%[_c]), %%xmm2;\n"
+		// "movss (%[_d]), %%xmm3;\n"
+		// "movss (%[_e]), %%xmm4;\n"
+		// "movss (%[_f]), %%xmm5;\n"
 
 		:
 
-		:
-
+		:	//input
+			// [_a] "r" (&a),
+		 //    [_b] "r" (&b),
+		 //    [_c] "r" (&c),
+		 //    [_d] "r" (&d),
+		 //    [_e] "r" (&e),
+		 //    [_f] "r" (&f)
 		:
 		"cc","memory","rax","rdx","rcx","rbx","rsi","rdi","r8","r9","r10",
-		"r11","r12","r13","r14","r15"
+		"r11","r12","r13","r14","r15","xmm0","xmm1","xmm2","xmm3","xmm4","xmm5"
 	);
 	raise(SIGSEGV);
 }
